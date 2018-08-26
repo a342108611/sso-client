cd9dfbbb96d98298222f1dee556f8ebd75e72e30
zhu yong jun <tmzyj2014@163.com>
2018-08-26 22:21:33 +0800

    完成单点登陆客户端demo

--- src/main/java/com/sangfor/sso/filter/SSOClientFilter.java
+++ src/main/java/com/sangfor/sso/filter/SSOClientFilter.java
@@ -1,6 +1,8 @@
 package com.sangfor.sso.filter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -12,7 +14,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.sangfor.sso.util.HttpUtils;
 import com.sangfor.sso.util.SSOClientUtil;
+
+import net.sf.json.JSONObject;
 
 public class SSOClientFilter implements Filter{
 
@@ -32,6 +39,32 @@ public class SSOClientFilter implements Filter{
 			chain.doFilter(req, resp);
 			return;
 		}
+		// 判断地址栏中是否有token
+		String token = req.getParameter("token");
+		System.out.println("token:"+token);
+		if (StringUtils.isNoneBlank(token)) {
+			// 判断token是否由谁中心产生
+			String url = SSOClientUtil.SERVER_URL_PREFIX + "/sso/verify";
+			Map<String, String> params = new HashMap<String, String>();
+			params.put("token", token);
+			try {
+				String isVerify = HttpUtils.sendPostRequest(url, params, null);
+				System.out.println(isVerify);
+				System.out.println("yes".equals(isVerify) );
+				JSONObject fromObject = JSONObject.fromObject(isVerify);
+				System.out.println(fromObject.get("status"));
+				Integer status = (Integer)fromObject.get("status");
+				if (status == 200) {
+					System.out.println("进入登陆成功");
+					session.setAttribute("isLogin", true);
+					chain.doFilter(req, resp);
+					return;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
 		SSOClientUtil.redirectToSSOURL(req, resp);
 	}
 
